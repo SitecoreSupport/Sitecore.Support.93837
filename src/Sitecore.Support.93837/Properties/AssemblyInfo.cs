4e139c0eb33de9d1ce602da18119fba76baa83dd
DK\ala <ala@sitecore.net>
2017-04-04 14:26:54 +0300

    first commit

---
+++ src/Sitecore.Support.93837/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.93837")]
+[assembly: AssemblyProduct("Sitecore.Support.93837")]
+[assembly: ComVisible(false)]
